cc6616d36432f14709f5620f5d7c9895c64aeacd
4ndryx <andlypierre@gmail.com>
2021-05-21 18:48:09 -0400

    heroku db set up

--- models/connection.php
+++ models/connection.php
@@ -1,8 +1,13 @@
 <?php 
 
 function con(){
+	$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+	$cleardb_server = $cleardb_url["host"];
+	$cleardb_username = $cleardb_url["user"];
+	$cleardb_password = $cleardb_url["pass"];
+	$cleardb_db = substr($cleardb_url["path"],1);
 	try {
-		$connection = new PDO('mysql:host=localhost;dbname=administracion_arriendos', 'root', '');
+		$connection = new PDO("mysql:host=$cleardb_server;dbname=$cleardb_db", "$cleardb_username", "$cleardb_password");
 		return $connection;
 		
 	} catch (PDOException $e) {
