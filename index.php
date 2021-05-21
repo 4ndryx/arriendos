36a8f5bdb4db03ea7cb22a3506f8a0156f5cbf22
4ndryx <andlypierre@gmail.com>
2021-05-21 17:33:26 -0400

    Path change

--- index.php
+++ index.php
@@ -1,11 +1,3 @@
-<?php session_start();
-require 'public/config.php';
-require FOLDER.'controllers/session.php';
-
-verifySession();
-
-	$content = '';
-	$title = 'Blank page';
-	$style = '';
-	$script = '';
-require FOLDER.'views/templates.php'; ?>
+<?php
+echo realpath('index.php');
+ ?>
