eaeb17525ddc835f2a52de317801efbb043adaaf
4ndryx <andlypierre@gmail.com>
2021-05-22 00:26:54 -0400

    heroku db set

--- views/header.php
+++ views/header.php
@@ -82,14 +82,14 @@
                         <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                             <div class="header-top-wraper">
                                 <div class="row">
-                                    <div class="col-lg-1 col-md-0 col-sm-1 col-xs-12 ml-4">
+                                    <div class="col-1">
                                         <div class="menu-switcher-pro">
                                             <button type="button" id="sidebarCollapse" class="btn bar-button-pro header-drl-controller-btn btn-info navbar-btn">
                                                     <i class="educate-icon educate-nav"></i>
                                                 </button>
                                         </div>
                                     </div>
-                                    <div class="col-lg-5 col-md-7 col-sm-6 col-xs-12">
+                                    <div class="col-5">
                                         <div class="header-top-menu tabl-d-n">
                                             <ul class="nav navbar-nav mai-top-nav">
                                                 <li class="nav-item"><a href="<?php echo LINK ?>/controllers/dashboard.php" class="nav-link">Inicio</a>
@@ -104,7 +104,7 @@
                                             </ul>
                                         </div>
                                     </div>
-                                    <div class="col-lg-5 col-md-5 col-sm-12 col-xs-12">
+                                    <div class="col-5">
                                         <div class="header-right-info">
                                             <ul class="nav navbar-nav mai-top-nav header-right-menu">
                                                 <!-- <li class="nav-item"><a href="#" data-toggle="dropdown" role="button" aria-expanded="false" class="nav-link dropdown-toggle"><span class="indicator-nt"></span></a>
--- views/show_lessee.view.php
+++ views/show_lessee.view.php
@@ -49,7 +49,7 @@ $contentH ='Lista de arrendatarios'?>
 
 
 <?php ob_start(); ?>
-<div class="card" style="max-width: 1000px; min-width: 1000px;">
+<div class="card" style="max-width: 100%; min-width: 100%;">
       <div class="card-header card-warning card-outline">
                 <h3 class="card-title">Arrendatarios registrados</h3>
               </div>
