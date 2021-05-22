33093708175ed4aefea82391cbe9cec9e042a511
4ndryx <andlypierre@gmail.com>
2021-05-21 23:03:01 -0400

    heroku db set

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'a2bf21320c21d2db3599c41ecb3e717866f152e0',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'a2bf21320c21d2db3599c41ecb3e717866f152e0',
+    ),
+  ),
+);
