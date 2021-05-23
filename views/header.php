8345ddd3fb942340489154771ab34b74898abb53
4ndryx <andlypierre@gmail.com>
2021-05-23 00:40:40 -0400

    heroku db set

--- views/header.php
+++ views/header.php
@@ -55,8 +55,8 @@
                         <li>
                             <a class="has-arrow" href="#" aria-expanded="false"><span class="fa fa-file-text-o"></span> <span class="mini-click-non">Arrendamientos</span></a>
                             <ul class="submenu-angle" aria-expanded="false">
-                                <li><a title="Ver Contratos" href="<?= LINK ?>controllers/show_contract"><span class="fa fa-list"> Ver todos</span></a></li>
-                                <li><a title="Agregar Contratos" href="<?= LINK ?>controllers/add_contract"><span class="fa fa-plus-circle"> Nuevo contrato</span></a></li>
+                                <li><a title="Ver Contratos" href="<?= LINK ?>controllers/show_contract.php"><span class="fa fa-list"> Ver todos</span></a></li>
+                                <li><a title="Agregar Contratos" href="<?= LINK ?>controllers/add_contract.php"><span class="fa fa-plus-circle"> Nuevo contrato</span></a></li>
                             </ul>
                         </li>
                         <li>
@@ -115,66 +115,7 @@
                                     <div class="col-5">
                                         <div class="header-right-info">
                                             <ul class="nav navbar-nav mai-top-nav header-right-menu">
-                                                <!-- <li class="nav-item"><a href="#" data-toggle="dropdown" role="button" aria-expanded="false" class="nav-link dropdown-toggle"><span class="indicator-nt"></span></a>
-                                                    <div role="menu" class="notification-author dropdown-menu animated zoomIn">
-                                                        <div class="notification-single-top">
-                                                            <h1>Notifications</h1>
-                                                        </div>
-                                                        <ul class="notification-menu">
-                                                            <li>
-                                                                <a href="#">
-                                                                    <div class="notification-icon">
-                                                                        <i class="educate-icon educate-checked edu-checked-pro admin-check-pro" aria-hidden="true"></i>
-                                                                    </div>
-                                                                    <div class="notification-content">
-                                                                        <span class="notification-date">16 Sept</span>
-                                                                        <h2>Advanda Cro</h2>
-                                                                        <p>Please done this project as soon possible.</p>
-                                                                    </div>
-                                                                </a>
-                                                            </li>
-                                                            <li>
-                                                                <a href="#">
-                                                                    <div class="notification-icon">
-                                                                        <i class="fa fa-cloud edu-cloud-computing-down" aria-hidden="true"></i>
-                                                                    </div>
-                                                                    <div class="notification-content">
-                                                                        <span class="notification-date">16 Sept</span>
-                                                                        <h2>Sulaiman din</h2>
-                                                                        <p>Please done this project as soon possible.</p>
-                                                                    </div>
-                                                                </a>
-                                                            </li>
-                                                            <li>
-                                                                <a href="#">
-                                                                    <div class="notification-icon">
-                                                                        <i class="fa fa-eraser edu-shield" aria-hidden="true"></i>
-                                                                    </div>
-                                                                    <div class="notification-content">
-                                                                        <span class="notification-date">16 Sept</span>
-                                                                        <h2>Victor Jara</h2>
-                                                                        <p>Please done this project as soon possible.</p>
-                                                                    </div>
-                                                                </a>
-                                                            </li>
-                                                            <li>
-                                                                <a href="#">
-                                                                    <div class="notification-icon">
-                                                                        <i class="fa fa-line-chart edu-analytics-arrow" aria-hidden="true"></i>
-                                                                    </div>
-                                                                    <div class="notification-content">
-                                                                        <span class="notification-date">16 Sept</span>
-                                                                        <h2>Victor Jara</h2>
-                                                                        <p>Please done this project as soon possible.</p>
-                                                                    </div>
-                                                                </a>
-                                                            </li>
-                                                        </ul>
-                                                        <div class="notification-view">
-                                                            <a href="#">View All Notification</a>
-                                                        </div>
-                                                    </div>
-                                                </li> -->
+                                                
                                                 <li class="nav-item">
                                                     <a href="#" data-toggle="dropdown" role="button" aria-expanded="false" class="nav-link dropdown-toggle">
                                                             <img src="" alt="" />
@@ -197,55 +138,5 @@
                     </div>
                 </div>
             </div>
-            <!-- Mobile Menu start -->
-           <!--  <div class="mobile-menu-area">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                            <div class="mobile-menu">
-                                <nav id="dropdown">
-                                    <ul class="mobile-menu-nav">
-                                        <li><a data-toggle="collapse" title="Dashboard" data-target="#Charts" href="#">Dashboard <span class="admin-project-icon edu-icon edu-down-arrow"></span></a>
-                                            <ul class="collapse dropdown-header-top">
-                                                <li><a href="<?php echo LINK ?>/controllers/dashboard.php" title="Dashboard" >Dashboard</a></li>
-                                            </ul>
-                                        </li>
-                                        <li><a data-toggle="collapse" title = "Arrendatarios" data-target="#demoevent" href="#">Arrendatarios <span class="admin-project-icon edu-icon edu-down-arrow"></span></a>
-                                            <ul id="demoevent" class="collapse dropdown-header-top">
-                                                <li><a href="<?= LINK ?>controllers/show_lessee.php" title = "Ver todos"><span>Ver todos</span></a></li>
-                                                <li><a href="<?= LINK ?>controllers/add_lesse.php" title = "Aggregar" ><span>Agregar arrendatarios</span></a></li>
-                                            </ul>
-                                        </li>
-                                        <li><a data-toggle="collapse" title = "Arrendadores" data-target="#demoevent" href="#">Arrendadores <span class="admin-project-icon edu-icon edu-down-arrow"></span></a>
-                                            <ul id="demoevent" class="collapse dropdown-header-top">
-                                                <li><a href="<?= LINK ?>controllers/show_lessor.php" title = "Ver todos"><span>Ver todos</span></a></li>
-                                                <li><a href="<?= LINK ?>controllers/add_lessor.php" title = "Aggregar" ><span>Agregar arrendadores</span></a></li>
-                                            </ul>
-                                        </li>
-                                        <li><a data-toggle="collapse"title = "Propriedades"  data-target="#demopro" href="#">Propriedades <span class="admin-project-icon edu-icon edu-down-arrow"></span></a>
-                                            <ul id="demopro" class="collapse dropdown-header-top">
-                                                <li><a href="<?= LINK ?>controllers/show_property.php" title = "Ver todos"><span >Ver todos</span></a></li>
-                                                <li><a href="<?= LINK ?>controllers/add_property.php" title = "Aggregar" ><span >Agregar propriedad</span></a></li>
-                                            </ul>
-                                        </li>
-                                        <li><a data-toggle="collapse" title = "Arrendatarios"  data-target="#democrou" href="#">Arrendamiento <span class="admin-project-icon edu-icon edu-down-arrow"></span></a>
-                                            <ul id="democrou" class="collapse dropdown-header-top">
-                                                <li><a href="<?php echo LINK ?>controllers/show_contract" title = "Ver todos"><span >Ver todos</span></a></li>
-                                                <li><a href="<?php echo LINK ?>controllers/add_contract" title = "Aggregar" ><span >Nuevo contrato</span></a></li>
-                                            </ul>
-                                        </li>
-                                        <li><a data-toggle="collapse" data-target="#demolibra" href="#">Administradores <span class="admin-project-icon edu-icon edu-down-arrow"></span></a>
-                                            <ul id="demolibra" class="collapse dropdown-header-top">
-                                                <li><a href="<?php echo LINK ?>controllers/show_user.php"><span >Ver todos</span></a></li>
-                                                <li><a href="<?php echo LINK ?>controllers/add_user.php"><span >Agregar administrador</span></a></li>
-                                            </ul>
-                                        </li>
-                                    </ul>
-                                </nav>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div> -->
-            <!-- Mobile Menu end -->
+           
         </div>
