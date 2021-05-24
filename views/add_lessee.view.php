14aa31679855bd6871e80d857914958e8da9af0a
4ndryx <andlypierre@gmail.com>
2021-05-24 00:15:48 -0400

    heroku db set

--- views/add_lessee.view.php
+++ views/add_lessee.view.php
@@ -24,57 +24,57 @@ $contentH ='Agregar un arrendatario'?>
       <!-- form start -->
       <form class="form-horizontal" id = "addLesseeForm" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post" enctype = "multipart/form-data">
         <div class="card-body">
-        <div class="row mb-4">
+        <div class="row mb-2">
           <!-- Name input -->
           <div class="form-outline col-md-4">
             <label class="form-label" for="lrFName">Nombres</label><input type="text" id="FName" class="form-control" name="fname" placeholder = "Nombres" value="<?php echo $lesseeedit['fname'] ?>" />
-            <p style="font-size: 12px; color: gray;"><spam class = "error-input"></spam>  ej: Andly</p>
+            <p style="font-size: 12px; color: gray;"><spam class = "error-message error-input"></spam>  ej: Andly</p>
           </div>
           <div class="form-outline col-md-4">
             <label class="form-label" for="lrLName">Apellidos</label><input type="text" id="LName" class="form-control" name="lname" placeholder = "Apellidos" value="<?php echo $lesseeedit['lname'] ?>" />
-            <p style="font-size: 12px; color: gray;"><spam class = "error-input"></spam>  ej: Pierre</p>
+            <p style="font-size: 12px; color: gray;"><spam class = "error-message error-input"></spam>  ej: Pierre</p>
           </div>
           <div class="form-outline col-md-4">
             <label class="form-label" for="lrId">Cedula</label><input type="text" id="lrId" class="form-control" name="id" placeholder = "Cedula" value="<?php echo $lesseeedit['id'] ?>" />
-            <p style="font-size: 12px; color: gray;"><spam class = "error-input"></spam>  ej: 84604654</p>
+            <p style="font-size: 12px; color: gray;"><spam class = "error-message error-input"></spam>  ej: 84604654</p>
           </div>            
         </div>
-        <div class="row mb-4">
+        <div class="row mb-2">
           <!-- Username input -->
           <div class="form-outline col-md-4">
             <label class="form-label" for="lrCellPhone">Telefono Celular</label><input type="text" id="lrCellPhone" class="form-control" name="phone" placeholder = "Telefono Celular" value="<?php echo $lesseeedit['phone'] ?>" />
-            <p style="font-size: 12px; color: gray;"><spam class = "error-input"></spam>  ej: 04126526654</p>
+            <p style="font-size: 12px; color: gray;"><spam class = "error-message error-input"></spam>  ej: 04126526654</p>
             
           </div>
           <div class="form-outline col-md-4">
             <label class="form-label" for="lrHomePhone">Telefono de casa</label><input type="text" id="lrHomePhone" class="form-control" name="home_phone" placeholder = "Telefono de casa" value="<?php echo $lesseeedit['home_phone'] ?>" />
-            <p style="font-size: 12px; color: gray;"><spam class = "error-input"></spam>  ej: 02683524569</p>
+            <p style="font-size: 12px; color: gray;"><spam class = "error-message error-input"></spam>  ej: 02683524569</p>
           </div>
           <div class="form-outline col-md-4">
             <label class="form-label" for="lrEmail">Correo Electronico</label><input type="text" id="lrEmail" class="form-control" name="email" placeholder = "Correo Electronico" value="<?php echo $lesseeedit['email'] ?>" />
-            <p style="font-size: 12px; color: gray;"><spam class = "error-input"></spam>  ej: andlypierre@gmail.com</p>
+            <p style="font-size: 12px; color: gray;"><spam class = "error-message error-input"></spam>  ej: andlypierre@gmail.com</p>
           </div>
         </div>
-        <div class="row mb-4">
+        <div class="row mb-2">
           <!-- Name input -->
           <div class="form-outline col-md-4">
             <label class="form-label" for="lrAdress">Direccion</label><input type="text" id="lrAdress" class="form-control" name="adress" placeholder = "Direccion" value="<?php echo $lesseeedit['adress'] ?>" />
-            <p style="font-size: 12px; color: gray;"><spam class = "error-input"></spam>  ej: Calle, Localidad, Ciudad, Estado</p>
+            <p style="font-size: 12px; color: gray;"><spam class = "error-message error-input"></spam>  ej: Calle, Localidad, Ciudad, Estado</p>
           </div>
 
           <!-- Username input -->
           <div class="form-outline col-md-4">
             <label class="form-label" for="lrOcupation">Ocupacion</label><input type="text" id="lrOcupation" class="form-control" name="ocupation" placeholder = "Ocupacion" value="<?php echo $lesseeedit['ocupation'] ?>" />
-            <p style="font-size: 12px; color: gray;"><spam class = "error-input"></spam>  ej: Comerciante</p>
+            <p style="font-size: 12px; color: gray;"><spam class = "error-message error-input"></spam>  ej: Comerciante</p>
           </div>
 
           <!-- Name input -->
           <div class="form-outline col-md-4">
             <label class="form-label" for="lrnationality">Nacionalidad</label><input type="text" id="lrnationality" class="form-control" name="nacionality" placeholder = "Nacionalidad" value="<?php echo $lesseeedit['nacionality'] ?>" />
-            <p style="font-size: 12px; color: gray;"><spam class = "error-input"></spam>  ej: Venezolano</p>
+            <p style="font-size: 12px; color: gray;"><spam class = "error-message error-input"></spam>  ej: Venezolano</p>
           </div>
         </div>
-        <div class="row mb-4">
+        <div class="row mb-2">
           <div class="form-outline col-md-4">
             <label class="form-label" for="lrCivilState">Estado civil</label><input type="text" id="lrCivilState" class="form-control" name="civil_status" placeholder = "Estado civil" value="<?php echo $lesseeedit['civil_status'] ?>" />
             
@@ -103,12 +103,12 @@ $contentH ='Agregar un arrendatario'?>
 
             <input type="hidden" name="oldImg" value="<?php echo $lesseeedit['img'] ?>">
             <div class="form-outline col-md-4">
-            <p style="font-size: 12px; color: gray;"><spam class = "error-input"></spam>  ej: Divorciado</p>
+            <p style="font-size: 12px; color: gray;"><spam class = "error-message error-input"></spam>  ej: Divorciado</p>
             
           </div>
 
         </div>
-                <div class="row error-message" style="color: red;"><p class="error-message"></p></div>
+                <div class="row error-message" style="color: red;"><p class="error-message  global-error"></p></div>
 
 <hr>
                                                      
