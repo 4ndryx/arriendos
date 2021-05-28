0c5dd2593a92fc0b4951dc0114f9d166d424ea2a
4ndryx <andlypierre@gmail.com>
2021-05-27 22:13:08 -0400

    heroku db set

--- views/header.php
+++ views/header.php
@@ -16,6 +16,7 @@
   <!-- Modal content -->
   <div class="modal-content" style="width: 25%; background-color: currentColor;">
     <span class="close close-Btn">&times;</span>
+    <p>Borrar registro?</p>
      <div class="modal-footer justify-content-between ">
       <button id = "cancel" type="button" class="btn btn-outline-light close-Btn" data-dismiss="modal">Cancelar</button>
       <button id ="delete" type="button" class="btn btn-outline-light">Borrar</button>
