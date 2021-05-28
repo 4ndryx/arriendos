29718e86e583ff1c7d7f72e2ed805702f88387e1
4ndryx <andlypierre@gmail.com>
2021-05-28 18:03:06 -0400

    heroku db set

--- public/js/jqscript.js
+++ public/js/jqscript.js
@@ -848,9 +848,9 @@ $('#Email').blur(function(e){
 function validateOthers(){
 			$('.btn-primary').removeAttr('disabled');
 	for(var i=0; i<$('input').length-3;i++){
-		if ($.trim($($('input')[i]).val()) == '' && $($($($('input')[i]).next()[0]).children()[1]).text() !== '' ){
+		if ($.trim($($('input')[i]).val()) == '' && $($($($('input')[i]).next()[0]).children()[1]).text() != '' ){
 			$('.btn-primary').attr('disabled', 'disabled');
-			console.log('disabled');				
+			console.log('disabled');
 				}
 	}
 }
