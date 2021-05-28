ceade85e4717cf04169d32f4352ffafae801610d
4ndryx <andlypierre@gmail.com>
2021-05-27 21:24:54 -0400

    heroku db set

--- controllers/session.php
+++ controllers/session.php
@@ -6,7 +6,7 @@ function verifySession(){
 		$lastAccess = $SESSION['lastAccess'];
 		$now = date('Y-n-j H:i:s');
 		$elapsedTime = (strtotime($now)-strtotime($lastAccess));
-		$ST = 300; // tirmpo de duracion de la seccion en segundos
+		$ST = 30000000; // tirmpo de duracion de la seccion en segundos
 
 		if($elapsedTime>= $ST){
 			session_destroy();
