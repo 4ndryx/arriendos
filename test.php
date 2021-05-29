ba703a11fcf490a351447850a0db75eee96dd961
4ndryx <andlypierre@gmail.com>
2021-05-29 14:13:19 -0400

    Purge cache

--- test.php
+++ test.php
@@ -1,12 +1,12 @@
 <?php require 'vendor/autoload.php';
 use Mailgun\Mailgun;
 # Instantiate the client.
-$mgClient = new Mailgun('YOUR_API_KEY');
-$domain = "YOUR_DOMAIN_NAME";
+$mgClient = new Mailgun('d2cb18512bad1157edd2d1c8148c923a-fa6e84b7-a7a171d0');
+$domain = "sandbox8f266bd3780543f7b2895f3f0743ea1f.mailgun.org";
 # Make the call to the client.
 $result = $mgClient->sendMessage($domain, array(
-	'from'	=> 'Excited User <mailgun@YOUR_DOMAIN_NAME>',
-	'to'	=> 'Baz <YOU@YOUR_DOMAIN_NAME>',
+	'from'	=> 'Excited User <mailgun@sandbox8f266bd3780543f7b2895f3f0743ea1f.mailgun.org>',
+	'to'	=> 'Baz <YOU@sandbox8f266bd3780543f7b2895f3f0743ea1f.mailgun.org>',
 	'subject' => 'Hello',
 	'text'	=> 'Testing some Mailgun awesomness!'
 )); ?>
