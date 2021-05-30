93959771936f7db662b87f846d5fa57c07069b03
4ndryx <andlypierre@gmail.com>
2021-05-29 21:57:43 -0400

    Purge cache

--- views/head.php
+++ views/head.php
@@ -9,32 +9,84 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- favicon
 		============================================ -->
-    <link rel="shortcut icon" type="image/x-icon" href="<?php echo LINK; ?>public/img/logo/logosn.png">
+    <!-- <link rel="shortcut icon" type="image/x-icon" href="<?php echo LINK; ?>public/img/logo/logosn.png"> -->
     <!-- Bootstrap CSS
 		============================================ -->
-    <link rel="stylesheet" href="<?php echo LINK; ?>public/css/bootstrap.min.css">
+    <!-- <link rel="stylesheet" href="<?php echo LINK; ?>public/css/bootstrap.min.css"> -->
     <!-- Bootstrap CSS
 		============================================ -->
-    <link rel="stylesheet" href="<?php echo LINK; ?>public/css/font-awesome.min.css">
+    <!-- <link rel="stylesheet" href="<?php echo LINK; ?>public/css/font-awesome.min.css"> -->
     <!-- normalize CSS
 		============================================ -->
-    <link rel="stylesheet" href="<?php echo LINK; ?>public/css/normalize.css">
+    <!-- <link rel="stylesheet" href="<?php echo LINK; ?>public/css/normalize.css"> -->
     <!-- main CSS
 		============================================ -->
-    <link rel="stylesheet" href="<?php echo LINK; ?>public/css/main.css">
+    <!-- <link rel="stylesheet" href="<?php echo LINK; ?>public/css/main.css"> -->
     <!-- metisMenu CSS
 		============================================ -->
-    <link rel="stylesheet" href="<?php echo LINK; ?>public/css/style.css">
+    <!-- <link rel="stylesheet" href="<?php echo LINK; ?>public/css/style.css"> -->
         <!-- metisMenu CSS
         ============================================ -->
-    <link rel="stylesheet" href="<?php echo LINK; ?>public/css/metisMenu/metisMenu.min.css">
-    <link rel="stylesheet" href="<?php echo LINK; ?>public/css/metisMenu/metisMenu-vertical.css">
+    <!-- <link rel="stylesheet" href="<?php echo LINK; ?>public/css/metisMenu/metisMenu.min.css"> -->
+    <!-- <link rel="stylesheet" href="<?php echo LINK; ?>public/css/metisMenu/metisMenu-vertical.css"> -->
 
     <!-- modernizr JS
 		============================================ -->
+    <!-- <link rel="stylesheet" href="<?php echo LINK ?>public/css/admin-lte.min.css"> -->
+    <!-- <link rel="stylesheet" href="<?php echo LINK ?>public/css/sweetalert2-theme-bootstrap-4/bootstrap-4.min.css"> -->
+    <!-- <script src="<?php echo LINK; ?>public/js/vendor/modernizr-2.8.3.min.js"></script> -->
+    <!-- <?= $style ?> -->
+
+<!-- </head> -->
+<link rel="stylesheet" href="<?php echo LINK; ?>public/css/bootstrap.min.css">
+    <!-- Bootstrap CSS
+        ============================================ -->
+    <link rel="stylesheet" href="<?php echo LINK; ?>public/css/font-awesome.min.css">
+    <!-- owl.carousel CSS
+        ============================================ -->
+    <link rel="stylesheet" href="<?php echo LINK; ?>public/css/owl.carousel.css">
+    <link rel="stylesheet" href="<?php echo LINK; ?>public/css/owl.theme.css">
+    <link rel="stylesheet" href="<?php echo LINK; ?>public/css/owl.transitions.css">
+    <!-- animate CSS
+        ============================================ -->
+    <link rel="stylesheet" href="<?php echo LINK; ?>public/css/animate.css">
+    <!-- normalize CSS
+        ============================================ -->
+    <link rel="stylesheet" href="<?php echo LINK; ?>public/css/normalize.css">
+    <!-- meanmenu icon CSS
+        ============================================ -->
+    <link rel="stylesheet" href="<?php echo LINK; ?>public/css/meanmenu.min.css">
+    <!-- main CSS
+        ============================================ -->
+    <link rel="stylesheet" href="<?php echo LINK; ?>public/css/main.css">
+    <!-- educate icon CSS
+        ============================================ -->
+    <link rel="stylesheet" href="<?php echo LINK; ?>public/css/educate-custon-icon.css">
+    <!-- morrisjs CSS
+        ============================================ -->
+    <link rel="stylesheet" href="<?php echo LINK; ?>public/css/morrisjs/morris.css">
+    <!-- mCustomScrollbar CSS
+        ============================================ -->
+    <link rel="stylesheet" href="<?php echo LINK; ?>public/css/scrollbar/jquery.mCustomScrollbar.min.css">
+    <!-- metisMenu CSS
+        ============================================ -->
+    <link rel="stylesheet" href="<?php echo LINK; ?>public/css/metisMenu/metisMenu.min.css">
+    <link rel="stylesheet" href="<?php echo LINK; ?>public/css/metisMenu/metisMenu-vertical.css">
+    <!-- calendar CSS
+        ============================================ -->
+    <link rel="stylesheet" href="<?php echo LINK; ?>public/css/calendar/fullcalendar.min.css">
+    <link rel="stylesheet" href="<?php echo LINK; ?>public/css/calendar/fullcalendar.print.min.css">
+    <!-- style CSS
+        ============================================ -->
+    <link rel="stylesheet" href="<?php echo LINK; ?>public/css/style.css">
+    <!-- responsive CSS
+        ============================================ -->
+    <!-- <link rel="stylesheet" href="<?php echo LINK; ?>public/css/responsive.css"> -->
+    <!-- modernizr JS
+        ============================================ -->
     <link rel="stylesheet" href="<?php echo LINK ?>public/css/admin-lte.min.css">
     <link rel="stylesheet" href="<?php echo LINK ?>public/css/sweetalert2-theme-bootstrap-4/bootstrap-4.min.css">
     <?= $style ?>
-    <script src="<?php echo LINK; ?>public/js/vendor/modernizr-2.8.3.min.js"></script>
 
+    <script src="<?php echo LINK; ?>public/js/vendor/modernizr-2.8.3.min.js"></script>
 </head>
