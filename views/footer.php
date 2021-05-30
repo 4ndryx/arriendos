44de7ea212ba5e2c3f4d22e1724fae08023b76c7
4ndryx <andlypierre@gmail.com>
2021-05-29 22:26:03 -0400

    Purge cache

--- views/footer.php
+++ views/footer.php
@@ -3,7 +3,7 @@
                 <div class="row">
                     <div class="col-lg-12">
                         <div class="footer-copy-right">
-                            <p>Sistema SIADAR diseñado por 4ndryx disponible bajo licencia<br><a href="https://creativecommons.org/licenses/by/4.0">Creative Commons atribucion <img src="<?php echo LINK ?>public/img/by.png" width="80px;" alt="Creative Commons"> </a></p>
+                            <p>Sistema SIADAR diseñado por Andly (4ndryx) Pierre disponible bajo licencia<br><a href="https://creativecommons.org/licenses/by/4.0">Creative Commons atribucion <img src="<?php echo LINK ?>public/img/by.png" width="80px;" alt="Creative Commons"> </a></p>
                         </div>
                     </div>
                 </div>
@@ -14,86 +14,17 @@
 
     <!-- jquery
         ============================================ -->
-    <!-- <script src="<?php echo LINK ?>public/js/vendor/jquery-3.6.0.min.js"></script> -->
-    <!-- bootstrap JS
-        ============================================ -->
-    <!-- <script src="<?php echo LINK ?>public/js/bootstrap.min.js"></script> -->
-    <!-- meanmenu JS -->
-        ============================================
-    <!-- <script src="<?php echo LINK ?>public/js/jquery.meanmenu.js"></script> -->
-    <!-- metisMenu JS
-        ============================================ -->
-    <!-- <script src="<?php echo LINK ?>public/js/metisMenu/metisMenu.min.js"></script> -->
-    <!-- <script src="<?php echo LINK ?>public/js/metisMenu/metisMenu-active.js"></script> -->
-    <!-- main JS
-        ============================================ -->
-    <!-- <script src="<?php echo LINK ?>public/js/main.js"></script> -->
-    <!-- tawk chat JS
-        ============================================ -->
-    <!-- <script src="<?php echo LINK ?>public/js/jqscript.js"></script> -->
-    <!-- SweetAlert2 -->
-    <!-- <script src="<?php echo LINK ?>public/js/sweetalert2/sweetalert2.min.js"></script> -->
-    <!-- <script src="<?php echo LINK ?>public/js/sweetalert2/swal.active.js"></script> -->
-    // <?= $script ?>
-<!-- </body> -->
-
-<!-- </html> -->
-
-    <!-- jquery
-        ============================================ -->
     <script src="<?php echo LINK ?>public/js/vendor/jquery-3.6.0.min.js"></script>
     <!-- bootstrap JS
         ============================================ -->
     <script src="<?php echo LINK ?>public/js/bootstrap.min.js"></script>
-    <!-- wow JS
-        ============================================ -->
-    <script src="<?php echo LINK ?>public/js/wow.min.js"></script>
-    <!-- price-slider JS
-        ============================================ -->
-    <script src="<?php echo LINK ?>public/js/jquery-price-slider.js"></script>
-    <!-- meanmenu JS
-        ============================================ -->
+    <!-- meanmenu JS -->
+        <!-- ============================================ -->
     <script src="<?php echo LINK ?>public/js/jquery.meanmenu.js"></script>
-    <!-- owl.carousel JS
-        ============================================ -->
-    <script src="<?php echo LINK ?>public/js/owl.carousel.min.js"></script>
-    <!-- sticky JS
-        ============================================ -->
-    <script src="<?php echo LINK ?>public/js/jquery.sticky.js"></script>
-    <!-- scrollUp JS
-        ============================================ -->
-    <script src="<?php echo LINK ?>public/js/jquery.scrollUp.min.js"></script>
-    <!-- counterup JS
-        ============================================ -->
-    <script src="<?php echo LINK ?>public/js/counterup/jquery.counterup.min.js"></script>
-    <!-- <script src="<?php echo LINK ?>public/js/counterup/waypoints.min.js"></script> -->
-    <script src="<?php echo LINK ?>public/js/counterup/counterup-active.js"></script>
-    <!-- mCustomScrollbar JS
-        ============================================ -->
-    <script src="<?php echo LINK ?>public/js/scrollbar/jquery.mCustomScrollbar.concat.min.js"></script>
-    <script src="<?php echo LINK ?>public/js/scrollbar/mCustomScrollbar-active.js"></script>
     <!-- metisMenu JS
         ============================================ -->
     <script src="<?php echo LINK ?>public/js/metisMenu/metisMenu.min.js"></script>
     <script src="<?php echo LINK ?>public/js/metisMenu/metisMenu-active.js"></script>
-    <!-- morrisjs JS
-        ============================================ -->
-<!--     <script src="<?php echo LINK ?>public/js/morrispublic/js/raphael-min.js"></script>
- --><!--     <script src="<?php echo LINK ?>public/js/morrispublic/js/morris.js"></script>
- --><!--     <script src="<?php echo LINK ?>public/js/morrispublic/js/morris-active.js"></script>
- -->    <!-- morrisjs JS
-        ============================================ -->
-    <script src="<?php echo LINK ?>public/js/sparkline/jquery.sparkline.min.js"></script>
-    <script src="<?php echo LINK ?>public/js/sparkline/jquery.charts-sparkline.js"></script>
-    <script src="<?php echo LINK ?>public/js/sparkline/sparkline-active.js"></script>
-    <!-- calendar JS
-        ============================================ -->
-    <script src="<?php echo LINK ?>public/js/calendar/moment.min.js"></script>
-    <script src="<?php echo LINK ?>public/js/calendar/fullcalendar.min.js"></script>
-    <script src="<?php echo LINK ?>public/js/calendar/fullcalendar-active.js"></script>
-    <!-- plugins JS
-        ============================================ -->
-    <script src="<?php echo LINK ?>public/js/plugins.js"></script>
     <!-- main JS
         ============================================ -->
     <script src="<?php echo LINK ?>public/js/main.js"></script>
@@ -103,5 +34,7 @@
     <!-- SweetAlert2 -->
     <script src="<?php echo LINK ?>public/js/sweetalert2/sweetalert2.min.js"></script>
     <script src="<?php echo LINK ?>public/js/sweetalert2/swal.active.js"></script>
-    <!-- Toastr -->
-    <script src="<?php echo LINK ?>public/js/toastr/toastr.min.js"></script>
+    <?= $script ?>
+</body>
+
+</html>
